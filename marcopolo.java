5588ce30a1b528e9d187966972f516a0370dcc58
Justin Baum <justinba1010@gmail.com>
2017-08-24 11:08:36 -0400

    Initialize

---
+++ marcopolo.java
@@ -0,0 +1,47 @@
+//Dr. JJ Shepherd
+from java.util import Random;
+public class MarcoPolo {
+
+  public static final int BOARD_SIZE = 10; //Board Size
+
+  //Enumerations
+  enum Spaces {
+    Empty,
+    Player,
+    Goal,
+    Walked_Path
+  };
+
+  public static void main(String[] args) {
+
+    int numberOfMoves = 0; //Counter
+    int pX = 0; //Positon Horizontal
+    int pY = 0; //Position Vertical
+    Random r = new Random();
+
+    //Get random variable for goals
+    int gX = r.nextInt(BOARD_SIZE);
+    int gY = r.nextInt(BOARD_SIZE);
+
+    Scanner keyboard = new Scanner(System.in); //Get input
+
+    Spaces[][] board  = new Spaces[BOARD_SIZE][BOARD_SIZE]; //Board
+
+    //Set board to Empty
+    for(int i = 0; i < board.length; i++) {
+      for(int j = 0; j < board[i].length, j++) {
+        board[i][j] = Spaces.Empty;
+      }
+    }
+    //Welcome user
+    System.out.println("Hey you\nHow goes it?\nLet's play the Marco Polos.");
+
+    boolean gameOver = false;
+    
+    while(not gameOver) {
+
+    }
+
+
+  }
+}
