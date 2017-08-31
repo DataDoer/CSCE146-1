31d880d94e8a42c7c35bc22a8509e32007a89d9c
Justin Baum <justinba1010@gmail.com>
2017-08-31 10:57:06 -0400

    Class for 9/31

--- UndergraduateRecordSystem.java
+++ UndergraduateRecordSystem.java
@@ -6,9 +6,8 @@ public class UndergraduateRecordSystem {
 
   //Record ugrads as file from array of objects
   public static void recordToFile(Ugrad[] uGrads, String fileName, boolean append) {
-    if(uGrads == null || fileName == null) {
-      return;
-    }
+    if(uGrads == null || fileName == null) return;
+
     try {//If failure, goes to catch
       PrintWriter fileWriter =
                   new PrintWriter(new FileOutputStream(fileName, append));
@@ -29,7 +28,50 @@ public class UndergraduateRecordSystem {
 
   //Return ugrads as array of objects from file
   public static Ugrad[] readFromFile(String fileName) {
-    //TODO
+    //Classic data parse
+    if(fileName == null) return null;
+
+    try {
+      Scanner fileScanner = new Scanner(new file(fileName)); //creates a file scanner
+
+      //TECHNIQUE
+      //Read the line, parse the line, do the data with the line
+
+      //Read header info
+      String fileLine = fileScanner.nextLine(); //Get first line
+      String splitLines = fileLine.split(DELIM); //Split line by delimiter
+      Ugrad[] uGrads;
+      //Verify file is not corrupted
+      //Read the header
+      if(splitLines.length == 2) { //Make sure header is uncorrupted
+        uGrads = new Ugrad[Integer.parseInt(splitLines[1])];
+      } else {
+        System.out.println("Bad file format");
+        return null;
+      }//if else
+
+      //Read the Body
+      int uGradCount = 0;
+      String name;
+      int id;
+      while(fileScanner.hasNextLine() && uGradCount < ugrads.length) { //Check to make sure correct number of ugrads
+        fileLine = fileScanner.nextLine(); //read line
+        splitLines = fileLine.split(DELIM); //split line
+        if(splitLines.length ==3) {//check right number of columns
+          //Parse it
+          name = splitLines[0];
+          id = Integer.parseInt(splitLines[1]);
+          uGrads[uGradCount] = new Ugrad(name,id,level);
+          uGradCount++;
+        }//if check lines
+      }//while
+
+      fileScanner.close()//always close
+      return uGrads; //return ugrads
+      
+    } catch (Exception e) {//Catch all
+      System.out.println(e);//let user know
+    }//catch
     return null;
   }//readFromFile
 
