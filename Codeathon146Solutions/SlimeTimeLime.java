b7e964f04f01ab56c77a86c496a283ff2cb87df3
Justin Baum <justinba1010@gmail.com>
2017-10-18 16:52:15 -0400

    Added comments for SlimeTimeLime

--- Codeathon146Solutions/SlimeTimeLime.java
+++ Codeathon146Solutions/SlimeTimeLime.java
@@ -11,6 +11,7 @@ public class Solution {
         int n = writer.nextInt();
         int x = writer.nextInt();
         writer.nextLine();
+        //So the way I went about doing this was two lists, one would take a "hash" of the word ENDINGS and another would take the counter of how many occurences we got of that
         ArrayList<BigInteger> words = new ArrayList<BigInteger>();
         ArrayList<BigInteger> counters = new ArrayList<BigInteger>();
         String word = "";
@@ -24,17 +25,19 @@ public class Solution {
             word = writer.nextLine();
             index = word.length()-x;
             for(int j = index; j < word.length(); j++) {
+                //This computes a hash of our word ending j-index gave us the index of what part of the ending we're at, and plus two(could be one) just incase we're 0 away
                 value = value.add(BigInteger.valueOf(word.codePointAt(j)*(7*(j-index+2))));
             }
-            index = words.indexOf(value);
-            if(index == -1) {
+            index = words.indexOf(value);//We check if this hash has already come up
+            if(index == -1) {//If it hasn't, then we add the hash to words and add one to the count of how many occurences, but because theres no rhymes with it yet, we can't add it to our total
                 words.add(value);
                 counters.add(one);
             } else {
+                //This could be a for loop of multiplication, this runs in O(n) every time, and the for loop runs in O(n) in worst time, so I didn't bother(lazy, already had this written)
                 value = counters.get(index);
-                counter = counter.add(value);
-                value = value.add(BigInteger.valueOf(1));
-                counters.set(index,value);
+                counter = counter.add(value);//Adds the number of previous words that had the same ending hash
+                value = value.add(BigInteger.valueOf(1));//Add one to the number of occurences
+                counters.set(index,value);//Add one to the list of counters, at that spot, meaning we had one more occurence of that rhyme and it rhymed with value-1 pairs
             }
 
         }
